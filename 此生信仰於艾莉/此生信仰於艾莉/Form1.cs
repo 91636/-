576203d72749a90b063bb2027299e2dc551e25ef
91636 <s1111636@syajh.tp.edu.tw>
2025-02-18 15:59:04 +0800

    Add files via upload

---
+++ 此生信仰於艾莉/此生信仰於艾莉/Form1.cs
@@ -0,0 +1,24 @@
+namespace 此生信仰於艾莉
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "要不要吃哈密哈";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                 }
+    }
+}
